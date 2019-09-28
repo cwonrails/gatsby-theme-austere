b6def63685db0ebe03e295fc78a696be9cbeecd5
Chris Watson <cwonrails@gmail.com>
2019-09-28 11:34:41 -0400

    add config & dotfiles to basic theme example

---
+++ examples/basic-with-config/gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Austere'
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-austere`,
+      options: {
+        basePath: '/writing'
+      }
+    },
+    'gatsby-plugin-theme-ui'
+  ]
+}
